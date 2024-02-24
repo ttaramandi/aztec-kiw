57d3979661d8bdefdd1748358c71103ff39584cb
José Pedro Sousa <outgoing@zpedro.dev>
2024-02-24 16:28:42 +0000

    conditionally deploying

--- docs/deploy_netlify.sh
+++ docs/deploy_netlify.sh
@@ -17,7 +17,7 @@ else
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $AZTEC_BOT_COMMENTER_GITHUB_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/AztecProtocol/aztec-packages/pulls/"$2"/files | \
+        "https://api.github.com/repos/AztecProtocol/aztec-packages/pulls/'$2'/files" | \
         jq '[.[] | select(.filename | startswith("docs/"))] | length > 0')
 
     if [ "$DOCS_CHANGED" = "false" ]; then
