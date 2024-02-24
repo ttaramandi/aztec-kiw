988c6a604802723dbecfc3c7ffdb2e7ca06d3c9d
José Pedro Sousa <outgoing@zpedro.dev>
2024-02-24 19:10:12 +0000

    conditionally deploying

--- docs/deploy_netlify.sh
+++ docs/deploy_netlify.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
-[ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
-set -eu
+# [ -n "${BUILD_SYSTEM_DEBUG:-}" ] && set -x # conditionally trace
+# set -eu
 
-extract_repo docs /usr/src extracted-repo
-cd extracted-repo/src/docs
-npm install netlify-cli -g
+# extract_repo docs /usr/src extracted-repo
+# cd extracted-repo/src/docs
+# npm install netlify-cli -g
 
-DEPLOY_OUTPUT=""
+# DEPLOY_OUTPUT=""
 
-# Check if we're on master
-if [ "$1" = "master" ]; then
-    # Deploy to production if the argument is "master"
-    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev --prod)
-else    
-    PR_URL="$2"                                                                                             INT ✘  17:02:39 
-    API_URL="${/github.com/api.github.com}"
-    API_URL="${API_URL/pull/repos}"
+# # Check if we're on master
+# if [ "$1" = "master" ]; then
+#     # Deploy to production if the argument is "master"
+#     DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev --prod)
+# else    
+    PR_URL="$2"
+    API_URL="${PR_URL/github.com/api.github.com/repos}"
+    API_URL="${API_URL/pull/pulls}"
     API_URL="${API_URL}/files"
 
     echo "API URL: $API_URL"
 
-    echo "https://api.github.com/repos/AztecProtocol/aztec-packages/pulls/"$2"/files"
-    DOCS_CHANGED=$(curl -L -f \
-        -H "Accept: application/vnd.github+json" \
+    DOCS_CHANGED=$(curl -L \
         -H "Authorization: Bearer $AZTEC_BOT_COMMENTER_GITHUB_TOKEN" \
         "${API_URL}" | \
         jq '[.[] | select(.filename | startswith("docs/"))] | length > 0')
+
+    echo "Docs changed: $DOCS_CHANGED"
 
     if [ "$DOCS_CHANGED" = "false" ]; then
         echo "No docs changed, not deploying"
         exit 0
     fi
 
-    # Regular deploy if the argument is not "master" and docs changed
-    DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev)
-    UNIQUE_DEPLOY_URL=$(echo "$DEPLOY_OUTPUT" | grep -E "https://.*aztec-docs-dev.netlify.app" | awk '{print $4}')
-    echo "Unique deploy URL: $UNIQUE_DEPLOY_URL"
+#     # Regular deploy if the argument is not "master" and docs changed
+#     DEPLOY_OUTPUT=$(netlify deploy --site aztec-docs-dev)
+#     UNIQUE_DEPLOY_URL=$(echo "$DEPLOY_OUTPUT" | grep -E "https://.*aztec-docs-dev.netlify.app" | awk '{print $4}')
+#     echo "Unique deploy URL: $UNIQUE_DEPLOY_URL"
 
-    extract_repo yarn-project /usr/src project
-    cd project/src/yarn-project/scripts
+#     extract_repo yarn-project /usr/src project
+#     cd project/src/yarn-project/scripts
 
-    yarn
-    UNIQUE_DEPLOY_URL=$UNIQUE_DEPLOY_URL yarn docs-preview
-fi
+#     yarn
+#     UNIQUE_DEPLOY_URL=$UNIQUE_DEPLOY_URL yarn docs-preview
+# fi
